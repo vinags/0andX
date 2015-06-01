5923c93fd917eb89026757a21f3abf8ff2106d96
Greg Vinall <gregvinall@Gregs-Macbook-Air.fritz.box>
2015-06-01 21:19:41 +1000

    Initial Commit

---
+++ OandX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OandX
+//
+//  Created by Greg Vinall on 1/06/2015.
+//  Copyright (c) 2015 Greg Vinall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ OandXTests/OandXTests.swift
@@ -0,0 +1,36 @@
+//
+//  OandXTests.swift
+//  OandXTests
+//
+//  Created by Greg Vinall on 1/06/2015.
+//  Copyright (c) 2015 Greg Vinall. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class OandXTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
